b66c3dfda7142bcb5917c0539f10ec5b0fa74b43
ali ghavidel <alighavidelma@gmail.com>
2025-08-27 23:23:04 +0330

    add CalculatorContextType into type

--- src/types/calculator.ts
+++ src/types/calculator.ts
@@ -2,3 +2,13 @@ export interface HistoryItem {
   expression: string;
   result: string;
 }
+
+export interface CalculatorContextType {
+  input: string;
+  history: HistoryItem[];
+  handleClick: (value: string) => void;
+  handleClear: () => void;
+  handleCalculate: () => void;
+  clearHistory: () => void;
+  setInput: (value: string) => void;
+}
