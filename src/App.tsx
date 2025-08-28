f00a33107cc0e0e0ff6d659db73c3a0df3f36820
ali ghavidel <alighavidelma@gmail.com>
2025-08-28 20:21:59 +0330

    integrate components  into App

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,15 @@
+import Display from "./components/Display";
+import History from "./components/History";
+import Keyboard from "./components/Keyboard";
+
 export default () => {
-  return <h1>test</h1>;
+  return (
+    <div className="flex items-center justify-center min-h-screen bg-gray-900">
+      <div className="bg-gray-800 p-6 rounded-2xl shadow-lg w-96">
+        <Display />
+        <Keyboard />
+        <History />
+      </div>
+    </div>
+  );
 };
